4d4b4527524b4d4eaf8c9ea80842da0bef785c8e
jdk-22 <bolthonkai7@gmail.com>
2024-06-13 22:43:29 +0700

    Sesi2

---
+++ Penggajian/pages/bagianubah.php
@@ -0,0 +1,19 @@
+<div id="top" class="row mb-3">
+    <div class="col">
+        <h3>Ubah data Bagian</h3>
+    </div>
+    <div class="col">
+        <a href="?page=bagian" class="btn btn-primary float-end">
+            <i class="fa fa-arrow-circle-left"></i>
+            Kembali
+        </a>
+    </div>
+</div>
+<div id="pesan" class="row mb-3">
+    <div class="col">
+        <?php
+        include "database/connection.php";
+        $id = $_GET['id'];
+        ?>
+    </div>
+</div>
